ebbb88d47e3180f89b853b3fc71d1e51836814a9
WOSHIXIMEI <yj12345602025@gmail.com>
2025-01-05 13:03:14 +0800

    Update config.example.js

--- js/config/config.example.js
+++ js/config/config.example.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
     API: {
-        KEY: 'YOUR_API_KEY_FROM_GOOGLE_AI_STUDIO',
+        KEY: 'AIzaSyCG_BuBPMSItTjzAWJhAljqpJ_NY0bzB8I',
         BASE_URL: 'wss://generativelanguage.googleapis.com/ws',
         VERSION: 'v1alpha',
         MODEL_NAME: 'models/gemini-2.0-flash-exp'
